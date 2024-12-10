03623404b0fd578eb1b716f6b21e0beace045bae
Vinod-brs <kingvinodkumart@gmail.com>
2024-12-10 14:02:43 +0530

    first commit

--- src/Home.js
+++ src/Home.js
@@ -5,8 +5,10 @@ import { Card } from "./core/Card";
 export const Home = () => {
   const [characterList, setCharacterList] = useState([]);
   const [searchList, setSearchList] = useState([]);
+  const [Load, setLoad] = useState(null);
 
   useEffect(() => {
+    setLoad('s');
     const getCharacters = async () => {
       try {
         let charList = await HttpGet();
@@ -15,11 +17,18 @@ export const Home = () => {
       } catch (error) {
         console.error(error);
       }
+      setLoad(null);
     }
     getCharacters();
+    
   }, [])
 
   const filterCharacters = (e) => {
+    if(e?.target?.value?.trim() === ""){
+      console.log('here')
+      return "";
+    }
+    
     let searchQuery = e?.target?.value.toLowerCase()
     if(searchQuery) {
       const filteredCharacters = characterList.filter((character) => {
@@ -33,9 +42,11 @@ export const Home = () => {
     } else {
       setSearchList(characterList);
     }
+    
   }
 
   const Characters = () => {
+    
     return (
       !!searchList.length && searchList.map((character, index) => {
         return (
@@ -56,13 +67,25 @@ export const Home = () => {
             <input onChange={filterCharacters} type="text" className="form-control" placeholder="Search for a character" aria-label="Search for a character" aria-describedby="img-search" autoFocus />
             <div className="input-group-prepend">
               <span className="input-group-text rounded-0 rounded-end"><img src="https://www.svgrepo.com/show/522443/search.svg" alt="" id="search"></img></span>
-              
             </div>
           </div>
         </div>
+        
         <div className="col col-10">
+          {
+            Load && 
+            <div className="text-center m-5 p-5">
+              <div className="spinner-grow text-info my-2 me-3 p-2" role="status"></div>
+              <div className="spinner-border text-info p-4" role="status"></div>
+              <div className="spinner-grow text-info my-2 ms-3 p-2" role="status"></div>
+            </div>
+          }
           <div className="input-group mb-3 charList">
             <Characters />
+            { !searchList.length &&
+              <h2 className="my-5 py-5 text-center text-danger">Results not found</h2>
+            }
+            
           </div>
         </div>
       </div>
